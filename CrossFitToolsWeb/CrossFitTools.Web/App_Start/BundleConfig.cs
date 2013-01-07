248c9b17f5dd7ce1fda918b138573f3a7b9569f4
cbertolasio <cbertolasio@gmail.com>
2013-01-06 18:22:19 -0700

    tidied up the calculator

--- CrossFitToolsWeb/CrossFitTools.Web/App_Start/BundleConfig.cs
+++ CrossFitToolsWeb/CrossFitTools.Web/App_Start/BundleConfig.cs
@@ -33,8 +33,6 @@ namespace CrossFitTools.Web
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/bootstrap.css",
                         "~/Content/bootstrap-responsive.css",
@@ -50,6 +48,8 @@ namespace CrossFitTools.Web
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
                         "~/Content/themes/base/jquery.ui.theme.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
         }
     }
 }
--- CrossFitToolsWeb/CrossFitTools.Web/Scripts/crossfitbenchmarks.calculator.js
+++ CrossFitToolsWeb/CrossFitTools.Web/Scripts/crossfitbenchmarks.calculator.js
@@ -12,18 +12,29 @@ CFBM.Calculator = (function () {
             var self = this,
                 base = .05,
                 weight = self.weight(),
-                value = 0;
+                value = 0,
+                roundingValue = $("input[name=roundingRadio]:checked").val();
 
             self.percentages.removeAll();
-
+            
             while (base < 1.05)
             {
-                value = weight * base
-
-                self.percentages.push({ label: parseInt(base * 100) + "%", value: parseInt(value) + " #" });
+                if (roundingValue === "toNearest5") {
+                    value = Math.round(parseInt((weight * base) / 5) * 5);
+                }
+                else if(roundingValue === "dontRound") {
+                    value = Math.round(weight * base);
+                }
+                
+                
+                self.percentages.push({ label: Math.round(base * 100) + "%", value: value + " #" });
 
                 base = base + .05;
             }
+        };
+
+        viewModel.valueIsRounded = function () {
+            return ($("input[name=roundingRadio]:checked").val() === "toNearest5");
         };
 
         viewModel.clear = function clear() {

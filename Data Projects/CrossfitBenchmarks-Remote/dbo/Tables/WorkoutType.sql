2e512d48fa6128c2c8f642f593df5e247ff0bb24
cbertolasio <cbertolasio@gmail.com>
2013-01-26 19:24:38 -0700

    removed data projects from this repo

--- Data Projects/CrossfitBenchmarks-Remote/dbo/Tables/WorkoutType.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[WorkoutType] (
-    [WorkoutTypeId] CHAR (1)      NOT NULL,
-    [Name]          NVARCHAR (50) NOT NULL,
-    CONSTRAINT [PK_WorkoutType] PRIMARY KEY CLUSTERED ([WorkoutTypeId] ASC)
-);
-

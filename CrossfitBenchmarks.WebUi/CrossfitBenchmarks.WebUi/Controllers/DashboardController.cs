74f99115f3ac986ecf66889a74d960a024581eff
cbertolasio <cbertolasio@gmail.com>
2013-02-26 01:03:16 -0700

    added dashboard controller and new views.  refactored main menu to use drop downs.  refactored index page to use new logos.

---
+++ CrossfitBenchmarks.WebUi/CrossfitBenchmarks.WebUi/Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrossfitBenchmarks.WebUi.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        public ActionResult Settings(){
+            return View();
+        }
+
+        public ActionResult Profile(){
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}

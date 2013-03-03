b6e49e2caabac11cdceceb943c34d93b1a7500d5
cbertolasio <cbertolasio@gmail.com>
2013-03-03 10:15:02 -0700

    added code to iontegrate logging controller with facebook open graph services.

---
+++ CrossfitBenchmarks.WebUi/CrossfitBenchmarks.WebUi/Services/IOpenGraphServices.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CrossfitBenchmarks.Data.DataTransfer;
+using System.Security.Principal;
+
+namespace CrossfitBenchmarks.WebUi.Services
+{
+    public interface IOpenGraphServices
+    {
+        void PublishAction(LogEntryDto dto, IIdentity identity);
+        
+    }
+}
---
+++ CrossfitBenchmarks.WebUi/CrossfitBenchmarks.WebUi/Services/OpenGraphService.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CrossfitBenchmarks.Data.DataTransfer;
+using System.Security.Principal;
+
+namespace CrossfitBenchmarks.WebUi.Services
+{
+    public class OpenGraphService : IOpenGraphServices
+    {
+        public OpenGraphService()
+        {
+
+        }
+        public void PublishAction(LogEntryDto dto, IIdentity identity)
+        {
+            
+        }
+    }
+}

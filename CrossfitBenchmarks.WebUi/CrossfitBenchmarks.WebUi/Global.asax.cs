1a6453b7027eb879d9fc59136f985fd9fed5767b
cbertolasio <cbertolasio@gmail.com>
2013-03-11 12:06:14 -0600

    added link for fb into footer, added version number into footer, added code to enable sliding cookie expiration

--- CrossfitBenchmarks.WebUi/CrossfitBenchmarks.WebUi/Global.asax.cs
+++ CrossfitBenchmarks.WebUi/CrossfitBenchmarks.WebUi/Global.asax.cs
@@ -5,6 +5,10 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.IdentityModel.Services;
+using System.IdentityModel;
+using System.IdentityModel.Tokens;
+using System.IdentityModel.Services.Configuration;
 
 namespace CrossfitBenchmarks.WebUi
 {
@@ -24,6 +28,21 @@ namespace CrossfitBenchmarks.WebUi
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+        }
+
+        void SessionAuthenticationModule_SessionSecurityTokenReceived(object sender, SessionSecurityTokenReceivedEventArgs e)
+        {
+            DateTime now = DateTime.UtcNow;
+            DateTime validFrom = e.SessionToken.ValidFrom;
+            DateTime validTo = e.SessionToken.ValidTo;
+            if ((now < validTo) &&
+            (now > validFrom.AddMinutes((validTo.Minute - validFrom.Minute) / 2))
+            ) {
+                SessionAuthenticationModule sam = sender as SessionAuthenticationModule;
+                e.SessionToken = sam.CreateSessionSecurityToken(e.SessionToken.ClaimsPrincipal, e.SessionToken.Context,
+                now, now.AddMinutes(10), e.SessionToken.IsPersistent);
+                e.ReissueCookie = true;
+            }
         }
     }
 }

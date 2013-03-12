13f929a8eb6a0895c8e701979c0f95c839146d68
cbertolasio <cbertolasio@gmail.com>
2013-03-12 01:12:38 -0600

    fxied fb integration issues, fixed user voice integration issues

--- CrossfitBenchmarks.WebUi/CrossfitBenchmarks.WebUi/Services/TheHerosOpenGraphAction.cs
+++ CrossfitBenchmarks.WebUi/CrossfitBenchmarks.WebUi/Services/TheHerosOpenGraphAction.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using CrossfitBenchmarks.Data.DataTransfer;
 using System.Security.Principal;
+using CrossfitBenchmarks.WebUi.Utility;
 
 namespace CrossfitBenchmarks.WebUi.Services
 {
@@ -21,9 +22,9 @@ namespace CrossfitBenchmarks.WebUi.Services
             var client = GetClient();
             var parameters = GetBaseParameters();
 
-            parameters["hero_workout"] = "http://crossfitbenchmarks.azurewebsites.net/facebookobjects/heroworkout";
+            parameters["hero_workout"] = String.Format("{0}{1}", Constants.RootDomain,"facebookobjects/heroworkout");
             parameters["og:type"] = "everywod:hero_workout";
-            parameters["og:url"] = "http://crossfitbenchmarks.azurewebsites.net/facebookobjects/heroworkout";
+            parameters["og:url"] = String.Format("{0}{1}", Constants.RootDomain, "facebookobjects/heroworkout");
             parameters["og:title"] = "Hero WOD";
             parameters["score"] = "220";
 

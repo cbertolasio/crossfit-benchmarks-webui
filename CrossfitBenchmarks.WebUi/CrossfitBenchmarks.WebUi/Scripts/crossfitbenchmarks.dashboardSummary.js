b1f28813431d9132acc6078f8fcd3c1a83a28a41
cbertolasio <cbertolasio@gmail.com>
2013-03-21 23:25:26 -0600

    refactored workoutHistory js into seperate file

--- CrossfitBenchmarks.WebUi/CrossfitBenchmarks.WebUi/Scripts/crossfitbenchmarks.dashboardSummary.js
+++ CrossfitBenchmarks.WebUi/CrossfitBenchmarks.WebUi/Scripts/crossfitbenchmarks.dashboardSummary.js
@@ -1,46 +1,5 @@
 ﻿/// <reference path="crossfitbenchmarks.site.js" />
 
-CFBM.namespace("CFBM.WorkoutHistory");
-CFBM.WorkoutHistory = (function () {
-    var $container;
-
-    function WorkoutHistoryViewModel(){
-        var self = this;
-        ko.mapping.fromJS(workoutHistoryViewModel, {}, this);
-
-        self.workoutName = ko.computed(function () {
-            if (self.value().length > 0) {
-                return self.value()[0].WorkoutName();
-            }
-
-            return "";
-
-        }, this);
-
-        self.workoutNameForHeader = ko.computed(function () {
-            if (self.value().length > 0)
-            {
-                return " for '" + self.workoutName() + "'";
-            }
-            
-            return "";
-
-        }, this);
-    }
-
-    function ready(rootContainer){
-        $container = rootContainer;
-
-        ko.applyBindings(new WorkoutHistoryViewModel(), $container[0]);
-
-        CFBM.Site.init();
-    };
-
-    return {
-        ready:ready
-    }
-})();
-
 CFBM.namespace("CFBM.Dashboard");
 CFBM.Dashboard = (function () {
     var viewModel = null,
@@ -135,14 +94,9 @@ CFBM.Dashboard = (function () {
 }());
 
 $(document).ready(function () {
-    var dashboardModule = CFBM.Dashboard,
-        workoutHistoryModule = CFBM.WorkoutHistory;
+    var dashboardModule = CFBM.Dashboard;
 
     if ($("#summary-content").length) {
         dashboardModule.ready($("#summary-content"));
-    }
-
-    if ($(".workoutHistory-container").length) {
-        workoutHistoryModule.ready($(".workoutHistory-container"));
     }
 });
---
+++ CrossfitBenchmarks.WebUi/CrossfitBenchmarks.WebUi/Scripts/crossfitbenchmarks.workoutHistory.js
@@ -0,0 +1,50 @@
+﻿/// <reference path="crossfitbenchmarks.site.js" />
+
+CFBM.namespace("CFBM.WorkoutHistory");
+CFBM.WorkoutHistory = (function () {
+    var $container;
+
+    function WorkoutHistoryViewModel(){
+        var self = this;
+        ko.mapping.fromJS(workoutHistoryViewModel, {}, this);
+
+        self.workoutName = ko.computed(function () {
+            if (self.value().length > 0) {
+                return self.value()[0].WorkoutName();
+            }
+
+            return "";
+
+        }, this);
+
+        self.workoutNameForHeader = ko.computed(function () {
+            if (self.value().length > 0)
+            {
+                return " for '" + self.workoutName() + "'";
+            }
+            
+            return "";
+
+        }, this);
+    }
+
+    function ready(rootContainer){
+        $container = rootContainer;
+
+        ko.applyBindings(new WorkoutHistoryViewModel(), $container[0]);
+
+        CFBM.Site.init();
+    };
+
+    return {
+        ready:ready
+    }
+})();
+
+$(document).ready(function () {
+    var workoutHistoryModule = CFBM.WorkoutHistory;
+
+    if ($(".workoutHistory-container").length) {
+        workoutHistoryModule.ready($(".workoutHistory-container"));
+    }
+});
